da1026ddb330ca952b18cfdd226668c7771d3314
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-05-26 20:05:53 -0300

    resolucao do exercicio 03

--- ex03.js
+++ ex03.js
@@ -1,1 +1,12 @@
-console.log("Primeiro Commit!");
+const prompt = require('prompt-sync')();
+console.clear();
+
+console.log("Exercício da quantidade de dias!");
+
+let qtdAnos = +prompt("Digite quantos anos a sua pessoa tem: ");
+let qtdMeses = +prompt("Digite quantos meses a sua pessoa tem: ");
+let qtdDias = +prompt("Digite quantos dias a sua pessoa tem: ");
+
+let totalDias = (qtdAnos * 365) + (qtdMeses * 30) + qtdDias;
+
+console.log(`A sua pessoa tem exatamente ${totalDias} dias vividos!`);
