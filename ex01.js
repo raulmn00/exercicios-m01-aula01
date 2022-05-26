efa8d7a3b6b9fb80e56ff762ae94fadbf738de85
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-05-26 19:44:53 -0300

    resolucao do exercicio 01

--- ex01.js
+++ ex01.js
@@ -1,1 +1,10 @@
-console.log("Primeiro commit!");
+const prompt = require("prompt-sync")();
+console.clear();
+console.log("Média ponderada!");
+
+let notaA = prompt("Digite a primeira nota: ");
+let notaB = prompt("Digite a segunda nota: ");
+
+let mediaPonderada = (notaA * 0.4) + (notaB * 0.6); 
+
+console.log(`A média ponderada do aluno foi de: ${mediaPonderada}`);
