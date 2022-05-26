3d1a3a8a6b1654dd7a28e5bfcbef28af61001c8a
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-05-26 19:59:55 -0300

    resolucao do exercicio 02

--- ex02.js
+++ ex02.js
@@ -7,6 +7,13 @@ let nomePeca1 = prompt("Digite o nome da primeira peça: ");
 let valorPeca1 = +prompt("Quanto custa a primeira peça: ");
 let qtdPeca1 = +prompt("Quantas unidades dessa peça você deseja? ");
 
-console.log(nomePeca1);
-console.log(valorPeca1);
-console.log(qtdPeca1);
+let nomePeca2 = prompt("Digite o nome da segunda peça: ");
+let valorPeca2 = +prompt("Quanto custa a segunda peça: ");
+let qtdPeca2 = +prompt("Quantas unidades dessa peça você deseja? ");
+
+let valorTotal1= valorPeca1 * qtdPeca1;
+let valorTotal2 = valorPeca2 * qtdPeca2;
+
+console.log(`Para a primeira peça ${nomePeca1}, você desejou comprar ${qtdPeca1} unidades e pagará a quantia de ${valorTotal1}`);
+console.log();
+console.log(`Para a primeira peça ${nomePeca2}, você desejou comprar ${qtdPeca2} unidades e pagará a quantia de ${valorTotal2}`);
